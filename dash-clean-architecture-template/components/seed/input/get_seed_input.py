cbc35706167e7c4a78c44d4e2df6492e0040307d
bot-nosense <uchihanosense@gmail.com>
2023-02-14 08:08:20 +0700

    create seed data

---
+++ dash-clean-architecture-template/components/seed/input/get_seed_input.py
@@ -0,0 +1,19 @@
+'''
+    step 1: lấy data từ file json
+    step 2: install package create seed data input SVRP v3
+'''
+
+import json
+
+with open("components\seed\input\seed.json", "r") as f:
+    seed_data_input_svrp_v3 = json.load(f)
+
+locations = seed_data_input_svrp_v3["locations"]
+vehicles = seed_data_input_svrp_v3["vehicles"]
+customers = seed_data_input_svrp_v3["customers"]
+matrix_config = seed_data_input_svrp_v3["matrixConfig"]
+depots = seed_data_input_svrp_v3["depots"]
+distances = seed_data_input_svrp_v3["distances"]
+requests = seed_data_input_svrp_v3["requests"]
+
+
