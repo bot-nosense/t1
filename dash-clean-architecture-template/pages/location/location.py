3c6189ef4d0302350deae601b8b5cda6c1a74d68
bot-nosense <uchihanosense@gmail.com>
2023-02-14 08:06:31 +0700

    create obj location

--- dash-clean-architecture-template/pages/location/location.py
+++ dash-clean-architecture-template/pages/location/location.py
@@ -1,7 +1,14 @@
 import json
+
 from dash import html
-from ..data import locations
+# from ..data import locations
+# from components.visualization.common import *
+from components.visualization.draw import * 
 
-json_string = json.dumps(locations)
+# json_string = json.dumps(locations)
 
-layout = html.Div(html.P(json_string))
+
+layout = html.Div( [
+    composition_of_locations(), # Pie chart
+
+])
