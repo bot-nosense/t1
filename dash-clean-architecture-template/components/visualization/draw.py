b30febcfc3386053bffd00192dd87584da77a3dd
bot-nosense <uchihanosense@gmail.com>
2023-02-15 18:07:28 +0700

    add color mapbox

--- dash-clean-architecture-template/components/visualization/draw.py
+++ dash-clean-architecture-template/components/visualization/draw.py
@@ -42,17 +42,30 @@ def composition_of_locations():
     return pie_chart.render_go()
 
 
+def random_color():
+    return "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+
+
 # map chart
 def point_on_the_map():
 
-    def set_color_scheme():
-        return 1
+    def set_color_scheme(location_type):
+        match location_type:
+            case "HUB": return 'green'
+            case "DEPOT": return'red'
+            case "STATION": return 'blue'
+            case "CUSTOMER": return 'orange'
+            case "SATELLITE": return 'purple'
+
+    # # def get_color_scheme(lat, long):
+    # #     return 1
 
 
     model = {
         'Name': [], # loaction type
         'Latitude': [],
-        'Longitude': []
+        'Longitude': [],
+        'Color': []
     }
 
     def create_data_model(locations, model):
@@ -60,31 +73,73 @@ def point_on_the_map():
             model['Name'].append(locations[i]['lTypes'][0])
             model['Latitude'].append(locations[i]['lat'])
             model['Longitude'].append(locations[i]['lng'])
+            model['Color'].append(set_color_scheme(locations[i]['lTypes'][0]))
 
         df = pd.DataFrame(model)
         return df
 
     data_model = create_data_model( locations, model )
 
-    data = go.Scattermapbox(
-        lat=list(data_model['Latitude']),
-        lon=list(data_model['Longitude']),
-        mode='markers+text',
-        marker=dict(size=10, color='green'),
-        textposition='top right',
-        textfont=dict(size=12, color='black'),
-        text=[data_model['Name'][i] for i in range(data_model.shape[0])]                                                                       
-    )
+    # data = go.Scattermapbox(
+    #     lat = list(data_model['Latitude']),
+    #     lon = list(data_model['Longitude']),
+    #     mode='markers+text',
+    #     marker=go.scattermapbox.Marker(
+    #         color=list(data_model['Color']),
+    #         size=9
+    #     ),
+    #     textposition='top right',
+    #     textfont= dict(size=12, color='black'),
+    #     text=[data_model['Name'][i] for i in range(data_model.shape[0])]                                                                       
+    # )
 
-    layout = dict(
+    # layout = dict(
+    #     margin=margin_default,
+    #     mapbox=dict(
+    #         accesstoken=mapbox_access_token,
+    #         center=locations_default['Ha Noi'],
+    #         style='light',
+    #         zoom = 7 )
+    # )
+
+    
+
+    fig = go.Figure()
+
+    for i in range(data_model.shape[0]):
+
+        fig.add_trace(go.Scattermapbox(
+            lat=[data_model['Latitude'][i]],
+            lon=[data_model['Longitude'][i]],
+            mode='markers+text',
+            marker=dict(
+                size=10,
+                color = data_model['Color'][i]
+            ),
+            text=data_model['Name'][i],
+            # hoverinfo='text',
+            textposition='top right',
+            textfont= dict(size=12, color='black'),
+        ))
+
+    fig.update_layout(
+        # title='Nuclear Waste Sites on Campus',
+        # autosize=True,
+        # hovermode='closest',
+        # showlegend=False,
         margin=margin_default,
         mapbox=dict(
             accesstoken=mapbox_access_token,
+            # bearing=0,
             center=locations_default['Ha Noi'],
-            style='light',
-            zoom = 7 )
+            # pitch=0,
+            zoom=7,
+            style='light'
+        ),
     )
-    map_chart = Scatter_Plots_on_Mapbox(data, layout)
 
-    return map_chart.render_go()
+    map_chart = Scatter_Plots_on_Mapbox(fig= fig)
+
+    return map_chart.render_go_trace()
+
 
