682f096963a9b9cda26ba1193c8b98574d7a89fd
Vu Doan Van <uchihanosense@gmail.com>
2023-03-15 07:18:20 +0700

    update page home

--- dash-clean-architecture-template/pages/home/home.py
+++ dash-clean-architecture-template/pages/home/home.py
@@ -1,8 +1,6 @@
-# import dash_html_components as html
 from dash import html
-# import input_svrp_v3.main as ip
 
-# input_data = ip.seed()
-
-layout = html.Div(html.P("This is the content of the Home page!"))
+layout = html.Div([
+     html.P("This is the content of the Home page!"),
+])
 
